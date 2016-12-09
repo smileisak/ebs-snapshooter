59e507ddcd6c00a305dacb03aa19294fede47673
ismailkaboubi <ismail.kaboubi@cy-play.com>
2016-12-09 11:32:56 +0100

    adding .travis.yam file

---
+++ ut.py
@@ -0,0 +1,10 @@
+import unittest
+
+class TestDateCompare(unittest.TestCase):
+
+    def test_date_compare(self):
+        self.assertEqual(1, 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
